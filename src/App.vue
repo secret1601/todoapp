bd9284113496677090bdd4e00a53966bc34ad6b0
secret <asdxxxcx41@gmail.com>
2022-04-05 16:46:05 +0900

    emit 및 props

--- src/App.vue
+++ src/App.vue
@@ -2,44 +2,13 @@
   <div class="container">
     <h1>TODO LIST</h1>
     <!-- 할일 추가 입력창 -->
-    <form action="" class="d-flex" v-on:submit.prevent="onSubmit">
-      <div class="flex-grow-1 mr-2">
-        <input 
-          type="text" 
-          placeholder="Type New Todo"
-          class="form-control"
-          v-model="todo"
-        >
-      </div>
-      <div>
-        <button class="btn btn-primary" type="submit">Add</button>
-      </div>
-    </form>
-    <!-- 내용 입력 안내창 -->
-    <div v-show="hasError" class="red">내용을 입력하세요.</div>
+    <TodoSimpleForm v-on:add-todo="addTodo"/>
+
+    <!-- 목록없음 안내창 -->
+    <div v-show="!todos.length" class="red">생성된 Todo 목록이 없습니다.</div>
     
     <!-- todo 목록창 -->
-    <div 
-      class="card mt-2"
-      v-for = "(todo, index) in todos"
-      v-bind:key = "todo.id"
-    >
-      <div class="card-body p-2 d-flex">
-
-        <div class="form-check flex-grow-1">
-          <input type="checkbox" class="form-check-input" v-model="todo.complete" v-bind:id="todo.id">
-          <label class="form-check-label" v-bind:class="{ todocss : todo.complete}" v-bind:for="todo.id"> 
-            {{ todo.subject }} 
-          </label>
-        </div>
-        <!-- 삭제버튼 -->
-        <div>
-          <button class="btn btn-danger btn-sm" @click="deleteTodo(index)">Delete</button>
-        </div>
-
-
-      </div>
-    </div>   
+    <TodoList v-bind:todos="todos"/>
 
   </div>
 </template>
@@ -47,20 +16,23 @@
 <script>
   import {ref} from 'vue';
 
+  import TodoSimpleForm from './components/TodoSimpleForm.vue'
+  import TodoList from './components/TodoList.vue'
+
   export default {
+
+    components: {
+      TodoSimpleForm,
+      TodoList
+    },
+
     setup(){ 
       // 할일 목록(배열)을 저장
       const todos = ref([
-        {id: 1, subject: '할일 1', complete: true},
-        {id: 2, subject: '할일 2', complete: false},
-        {id: 3, subject: '할일 3', complete: false},
-        {id: 4, subject: '할일 4', complete: false}
+        {id:1, subject:'할일', completed: false}
       ]);
-      // 추가할 할일
-      const todo = ref('');
+      
 
-      // 내용 입력 안내창 여부
-      const hasError = ref(false);
 
       // todo 스타일 시트
       const todoStyle = {
@@ -69,41 +41,22 @@
       }
 
       // 버튼 클릭시 처리
-      const onSubmit = () => {   
-        // 내용이 있는지 없는지 구분한다.
-        if(todo.value === '') {
-
-          //안내창 보여준다.
-          hasError.value = true;
-
-        }else{
-
-          // 안내창 숨기기
-          hasError.value = false;
-
-          // 할일의 내용을 배열로 추가한다.
-          todos.value.push({
-            id: Date.now(),
-            subject: todo.value,
-            complete: false
-          });
-
-        }
-
-        todo.value = '';
+      // TodoSimpleForm에서 add-todo 이벤트로 전달된 객체를
+      // 처리 해주는 콜백함수
+      const addTodo = (todo) => { 
+        // console.log(todo);
+        todos.value.push(todo);
       };
 
       const deleteTodo = (index) => {
-        console.log('지우기' + index);
+        // console.log('지우기' + index);
         // 배열내에서 순서 번호로 부터 1개 제거
         todos.value.splice(index, 1);
       }
 
       return {
         todos,
-        todo,
-        hasError,
-        onSubmit,
+        addTodo,
         todoStyle,
         deleteTodo
       }
