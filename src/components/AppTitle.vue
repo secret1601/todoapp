0285b3a1556a3c34caac8f4783564bf2615f59cb
secret <asdxxxcx41@gmail.com>
2022-04-11 13:21:34 +0900

    라우터

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>{{apptitle}}</h1>
+</template>
+
+<script>
+export default ({
+    // props: ['apptitle']
+    props: {
+        apptitle: {
+            type: String,
+            requierd: true
+        }
+    }
+})
+</script>
+
+<style>
+</style>
---
+++ src/pages/ProfileIndex.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>Profile page</h1>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+
+</style>
